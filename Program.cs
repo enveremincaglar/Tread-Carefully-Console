253bf72735072453dfd30a2eb1d410c177d807cb
enveremincaglar <enveremin@proton.me>
2024-10-03 00:47:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿Random random = new Random();
+
+// Variables
+int[] randomNumbers = new int[10];
+bool loopStop = false;
+int counter = 0;
+int startNumber = random.Next(1, 21);
+
+// Generate Numbers
+while (counter < 10)
+{
+    int currentNumber = random.Next(1, 21);
+
+    if (currentNumber != startNumber)
+    {
+        randomNumbers[counter] = currentNumber;
+        counter++;
+    }
+
+}
+
+// Game
+for (int i = 0; i <= 4; i++)
+{
+
+    Console.Write($"Enter {i + 1}. Number: ");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+
+    foreach (int number in randomNumbers)
+    {
+        if (number == userInput)
+        {
+
+            loopStop = true;
+            break;
+        }
+    }
+    if (loopStop == true)
+    {
+        break;
+    }
+}
+
+// Result
+if (loopStop == true)
+{
+    Console.WriteLine("You Lose.");
+}
+else
+{
+    Console.WriteLine("You Won");
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# TreadCarefullyGame
+
+## Overview
+
+TreadCarefullyGame is a simple console-based game where players must guess numbers generated randomly by the program. The objective is to avoid matching any of the randomly generated numbers with the player's guesses. If any guesses match, the player loses the game.
+
+## Features
+
+- Generates a random starting number.
+- Creates a list of 10 unique random numbers (excluding the starting number).
+- Allows the player to input their guesses.
+- Checks if the player's guesses match any of the generated numbers.
