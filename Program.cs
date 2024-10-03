8864bad13a94dec33e2bb8288c66ed8470cb4c65
enveremincaglar <enveremin@proton.me>
2024-10-03 12:28:11 +0300

    a few improvements

--- Program.cs
+++ Program.cs
@@ -3,28 +3,38 @@
 // Variables
 int[] randomNumbers = new int[10];
 bool loopStop = false;
-int counter = 0;
-int startNumber = random.Next(1, 21);
+int counter = 1;
+randomNumbers[0] = random.Next(1, 21);
 
 // Generate Numbers
 while (counter < 10)
 {
     int currentNumber = random.Next(1, 21);
+    bool addCheck = true;
 
-    if (currentNumber != startNumber)
+    foreach (int checkNumber in randomNumbers)
+    {
+        if (checkNumber == currentNumber)
+        {
+            addCheck = false;
+            break;
+        }
+    }
+    if (addCheck)
     {
         randomNumbers[counter] = currentNumber;
         counter++;
     }
-
 }
 
 // Game
-for (int i = 0; i <= 4; i++)
+for (int i = 0; i < 5; i++)
 {
 
     Console.Write($"Enter {i + 1}. Number: ");
     int userInput = Convert.ToInt32(Console.ReadLine());
+
+    loopStop = false;
 
     foreach (int number in randomNumbers)
     {
@@ -35,14 +45,14 @@ for (int i = 0; i <= 4; i++)
             break;
         }
     }
-    if (loopStop == true)
+    if (loopStop)
     {
         break;
     }
 }
 
 // Result
-if (loopStop == true)
+if (loopStop)
 {
     Console.WriteLine("You Lose.");
 }
