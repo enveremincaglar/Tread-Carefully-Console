a8c60f3965cd9501ef70d67e91fbe66658817f34
enveremincaglar <enveremin@proton.me>
2024-10-19 00:14:17 +0300

    added new Gameplay

---
+++ Program-Plus.cs
@@ -0,0 +1,195 @@
+﻿do
+{
+    PlayGame();
+} while (AskToPlayAgain());
+
+static void PlayGame()
+{
+    Console.Clear();
+    Random randomGenerator = new Random();
+    Dictionary<int, bool> mineStatues = new Dictionary<int, bool>();
+    List<int> userInputs = new List<int>();
+    List<int> trueSteps = new List<int>(); 
+
+    
+    for (int i = 1; i < 21; i++)
+    {
+        mineStatues[i] = false;
+    }
+
+    for (int i = 1; i < 21; i += 4)
+    {
+        mineStatues[randomGenerator.Next(i, i + 4)] = true;
+    }
+
+    Console.WriteLine("\t/----------------------[Start]---------------------\\");
+    for (int tableNumber = 1; tableNumber < 21; tableNumber++)
+    {
+        Console.Write($"\t {tableNumber} \t");
+
+        if (tableNumber % 4 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine("\t\\----------------------[Finish]--------------------/");
+
+    Console.WriteLine();
+
+    for (int repeatCount = 0; repeatCount < 5; repeatCount++) 
+    {
+        int checkedInput;
+        int lowerBound = repeatCount * 4 + 1; 
+        int upperBound = lowerBound + 3; 
+
+        while (true)
+        {
+            Console.Write($"\t{repeatCount + 1}. Choose which number your next step will be [ {lowerBound} - {upperBound} ]: ");
+            string inputStep = Console.ReadLine();
+            int step;
+
+            if (int.TryParse(inputStep, out step))
+            {
+                if (step >= lowerBound && step <= upperBound)
+                {
+                    checkedInput = step;
+                    break;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"\tPlease enter a number between {lowerBound} and {upperBound}.\n");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("\tInvalid input, please enter an integer.\n");
+                Console.ResetColor();
+            }
+        }
+        Console.Clear();
+
+        
+        userInputs.Add(checkedInput);
+
+        if (mineStatues[checkedInput] == true)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"\tYou hit a mine at {checkedInput}!");
+            Console.ResetColor();
+            DisplayFinalTable(mineStatues, userInputs);
+            Console.WriteLine("\tGame Over! You lost.");
+            return; 
+        }
+        else
+        {
+            
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.ResetColor();
+            trueSteps.Add(checkedInput); 
+        }
+
+        
+        if (trueSteps.Count == 5)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\tCongratulations! You've won the game!");
+            Console.ResetColor();
+            DisplayFinalTable(mineStatues, userInputs);
+            return; 
+        }
+
+        Console.WriteLine("\t/----------------------[Start]---------------------\\");
+        for (int i = 1; i < 21; i++)
+        {
+            if (trueSteps.Contains(i))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($"\t {i} \t");
+                Console.ResetColor();
+            }
+            else if (i == checkedInput && mineStatues[checkedInput] == true)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($"\t {checkedInput} \t");
+                Console.ResetColor();
+            }
+            else if (i == checkedInput)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($"\t {checkedInput} \t");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write($"\t {i} \t");
+            }
+
+            if (i % 4 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+        Console.WriteLine("\t\\----------------------[Finish]--------------------/");
+    }
+
+    if (trueSteps.Count == 5)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("\tCongratulations! You've won the game!");
+        Console.ResetColor();
+        DisplayFinalTable(mineStatues, userInputs);
+    }
+}
+
+static void DisplayFinalTable(Dictionary<int, bool> mineStatues, List<int> userInputs)
+{
+    Console.WriteLine("\n\t/----------------------[Final Table]---------------------\\");
+    for (int i = 1; i < 21; i++)
+    {
+        if (userInputs.Contains(i))
+        {
+            if (mineStatues[i])
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($"\t {i}* \t");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($"\t {i} \t");
+            }
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.Write($"\t {i} \t");
+        }
+
+        if (i % 4 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine("\t\\----------------------[Finish]--------------------/\n");
+}
+
+static bool AskToPlayAgain()
+{
+    Console.Write("\nWould you like to play again? (y/n): ");
+    string response = Console.ReadLine();
+    return response?.Trim().ToLower() == "y";
+}
--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 ## Overview
 
 TreadCarefullyGame is a simple console-based game where players must guess numbers generated randomly by the program. The objective is to avoid matching any of the randomly generated numbers with the player's guesses. If any guesses match, the player loses the game.
++ In the plus version of the game, you will encounter a different gameplay. A board will appear in front of you, and you will step on the mines one by one. If you step on the wrong mine, the game ends. Have fun!
 
-## Features
+## Note
 
-- Generates a random starting number.
-- Creates a list of 10 unique random numbers (excluding the starting number).
-- Allows the player to input their guesses.
-- Checks if the player's guesses match any of the generated numbers.
+Additionally, I could have written the program with more methods; yes, they are right. However, I didn't want to make it too complex. Plus, I was a bit lazy. I hope it helps someone. Enjoy the game!
+
+You can see two different types of boards; there are logical differences between the two, and the awareness I needed to create these differences was provided by ChatGPT. Thank you, ChatGPT!
+
+#### Saygılarımla ve minnetle, değerli öğretmenlerim Özge, Ahmet, Gürdal, Erkan ve Erdoğan; sizlerin desteği ve rehberliği benim için çok kıymetliydi. Teşekkürler! <3
